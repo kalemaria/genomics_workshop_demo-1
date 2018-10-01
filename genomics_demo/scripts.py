030585304d5e3e5c5ab5a0713a2b22850912c0c8
Maria Kalemanov <kalemanov@biochem.mpg.de>
2018-10-01 16:50:33 +0200

    Now use click in scripts.py, make it to command-line command 'complement'. Also added sphinx files.

--- genomics_demo/scripts.py
+++ genomics_demo/scripts.py
@@ -1,11 +1,16 @@
-import sys
-from dna import DNA
+import click
+from genomics_demo.dna import DNA
 
 
-def get_reverse_complement(seq):
-    return DNA(seq).compliment()
+@click.command()
+@click.argument('seq', type=str)
+@click.option('--reverse/--no-reverse', default=False, help="Whether to reverse or not")
+def get_reverse_complement(seq, reverse=True):
+    """Takes a DNA sequence and returns its complement"""
+    complement = str(DNA(seq).compliment())
+    if reverse:
+        complement = complement[::-1]
+    click.echo(complement)
 
 
-if __name__ == '__main__':
-    seq = sys.argv[1]
-    print(get_reverse_complement(seq))
+get_reverse_complement()  # call with python scripts.py GATC --reverse
---
+++ index.rst
@@ -0,0 +1,24 @@
+.. Genomics documentation master file, created by
+   sphinx-quickstart on Mon Oct  1 15:37:47 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Genomics's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Installation
+++++++++++++
+git install https://github.com/kalemaria/genomics_workshop_demo-1/
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
